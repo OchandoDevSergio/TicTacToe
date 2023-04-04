c9e9011a81f1eb2eac405824b208d3e2355cfb92
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-04 18:00:01 +0200

    hemos añadido un tablero lógico al backend en forma de array que contendría qué jugador a marcado qué posición

--- js/main.js
+++ js/main.js
@@ -4,29 +4,37 @@ let numeroClicks = 0;
 let turnosX = 0;
 let turnosO = 0;
 let esTurnoX = true;
-console.log("Es el turno de X")
+let ArrTablero = ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"];
+// console.log(ArrTablero);
+console.log("Es el turno de X");
 arrCasillas.map((casillaEscogida) => {
     casillaEscogida.addEventListener("click", () => {
     console.log(casillaEscogida.innerHTML);
     numeroClicks ++;
     // console.log("a ver los clicks", numeroClicks)
 
-    let esTurnoX = (numeroClicks%2 == 0)    //La variable esTurnoX será true si es el turno de X y false si es el turno de O
+    let esTurnoX = (numeroClicks%2 == 0)  //La variable esTurnoX será true si es el turno de X y
+    //false si es el turno de O
       ? true
       : false;
 
     //  console.log("valor de esTurnoX", esTurnoX);
 
-    if (esTurnoX == true) {
+    if (esTurnoX == true) {  //Con este if acumulamos los turnos que han transcurrido de cada jugador
+      //e indicamos por consola el turno actual
       console.log("Es el turno de X");
       turnosO ++;
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
+      ArrTablero[casillaEscogida.innerHTML] ="X";
+      console.log(ArrTablero);
     } else {
       console.log("Es el turno de O");
       turnosX ++;
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
+      ArrTablero[casillaEscogida.innerHTML] = "O";
+      console.log(ArrTablero);
     }
 
 
