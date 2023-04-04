c3f4e9b4d4e7876d4356da188728c9b47c3c2fc5
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-04 16:49:49 +0200

    two more steps in the game, showing the players turn and the number of turns each player already got

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 let conjuntoCasillas = document.getElementsByClassName("casilla");
 let arrCasillas = Array.from(conjuntoCasillas);
+let numeroClicks = 0;
+let turnosX = 0;
+let turnosO = 0;
+console.log("Es el turno de X")
 arrCasillas.map((casillaEscogida) => {
     casillaEscogida.addEventListener("click", () => {
     console.log(casillaEscogida.innerHTML);
+    numeroClicks ++;
+    // console.log("a ver los clicks", numeroClicks)
+
+    if (numeroClicks%2 == 0) {
+      console.log("Es el turno de X");
+      turnosO ++;
+      console.log(turnosX, "Son los turnos que X ha tenido.");
+      console.log(turnosO, "Son los turnos que O ha tenido.");
+    } else  {
+      console.log("Es el turno de O");
+      turnosX ++;
+      console.log(turnosX, "Son los turnos que X ha tenido.");
+      console.log(turnosO, "Son los turnos que O ha tenido.");
+    }
+
     });
   });
