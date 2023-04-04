26e550ee4a79a9360d9014a1c8b0829c3ded8dc5
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-04 17:23:46 +0200

    cambio en main.js para que el turno se contenga en una variable y no sólo aparezca en un console.log

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let arrCasillas = Array.from(conjuntoCasillas);
 let numeroClicks = 0;
 let turnosX = 0;
 let turnosO = 0;
+let esTurnoX = true;
 console.log("Es el turno de X")
 arrCasillas.map((casillaEscogida) => {
     casillaEscogida.addEventListener("click", () => {
@@ -10,17 +11,24 @@ arrCasillas.map((casillaEscogida) => {
     numeroClicks ++;
     // console.log("a ver los clicks", numeroClicks)
 
-    if (numeroClicks%2 == 0) {
+    let esTurnoX = (numeroClicks%2 == 0)    //La variable esTurnoX será true si es el turno de X y false si es el turno de O
+      ? true
+      : false;
+
+    //  console.log("valor de esTurnoX", esTurnoX);
+
+    if (esTurnoX == true) {
       console.log("Es el turno de X");
       turnosO ++;
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
-    } else  {
+    } else {
       console.log("Es el turno de O");
       turnosX ++;
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
     }
+
 
     });
   });
