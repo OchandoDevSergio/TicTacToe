99f78a3cc74c0f149ac78e9db3a2706c84ca101e
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-05 18:32:27 +0200

    ya declara un ganador a través de una función, pero faltaría cambiar la posición en que se llama a la función puesto que declara al ganador después de un turno más

--- js/main.js
+++ js/main.js
@@ -6,9 +6,76 @@ let turnosO = 0;
 let esTurnoX = true;
 let ArrTablero = ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"];
 // console.log(ArrTablero);
+
+
+const Comprobarganador = (ArrTablero) => {
+
+  if (((ArrTablero[0]=="X")&&(ArrTablero[1]=="X")&&(ArrTablero[2]=="X"))||((ArrTablero[3]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[5]=="X"))||((ArrTablero[6]=="X")&&(ArrTablero[7]=="X")&&(ArrTablero[8]=="X"))||((ArrTablero[0]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[8]=="X"))||((ArrTablero[2]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[6]=="X"))||((ArrTablero[0]=="X")&&(ArrTablero[3]=="X")&&(ArrTablero[6]=="X"))||((ArrTablero[1]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[7]=="X"))||((ArrTablero[2]=="X")&&(ArrTablero[5]=="X")&&(ArrTablero[8]=="X"))) {
+     console.log("El jugador X ha ganado.");
+    
+  } else if (((ArrTablero[0]=="O")&&(ArrTablero[1]=="O")&&(ArrTablero[2]=="O"))||((ArrTablero[3]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[5]=="O"))||((ArrTablero[6]=="O")&&(ArrTablero[7]=="O")&&(ArrTablero[8]=="O"))||((ArrTablero[0]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[8]=="O"))||((ArrTablero[2]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[6]=="O"))||((ArrTablero[0]=="O")&&(ArrTablero[3]=="O")&&(ArrTablero[6]=="O"))||((ArrTablero[1]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[7]=="O"))||((ArrTablero[2]=="O")&&(ArrTablero[5]=="O")&&(ArrTablero[8]=="O"))) {
+     console.log("El jugador O ha ganado.");
+
+  } else {
+     console.log("La partida continúa.")
+  }
+
+
+}
+
+
+// const Comprobarganador = (ArrTablero) => {
+
+//   if ((ArrTablero[0]=="X")&&(ArrTablero[1]=="X")&&(ArrTablero[2]=="X")) {
+//     console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[3]="X")&&(ArrTablero[4]="X")&&(ArrTablero[5]="X")) {
+//     console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[6]="X")&&(ArrTablero[7]="X")&&(ArrTablero[8]="X")) {
+//     console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[0]="X")&&(ArrTablero[4]="X")&&(ArrTablero[8]="X")) {
+//     console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[2]="X")&&(ArrTablero[4]="X")&&(ArrTablero[6]="X")) {
+//     console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[0]="X")&&(ArrTablero[3]="X")&&(ArrTablero[6]="X")) {
+//     console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[1]="X")&&(ArrTablero[4]="X")&&(ArrTablero[7]="X")) {
+//     console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[2]="X")&&(ArrTablero[5]="X")&&(ArrTablero[8]="X")) {
+//      console.log("El jugador X ha ganado.");
+//   } else if ((ArrTablero[0]="O")&&(ArrTablero[1]="O")&&(ArrTablero[2]="O")) {
+//     console.log("El jugador O ha ganado.");
+//   } else if ((ArrTablero[3]="O")&&(ArrTablero[4]="O")&&(ArrTablero[5]="O")) {
+//     console.log("El jugador O ha ganado.");
+//   } else if ((ArrTablero[6]="O")&&(ArrTablero[7]="O")&&(ArrTablero[8]="O")) {
+//     console.log("El jugador O ha ganado.");
+//   } else if ((ArrTablero[0]="O")&&(ArrTablero[4]="O")&&(ArrTablero[8]="O")) {
+//     console.log("El jugador O ha ganado.");
+//   } else if ((ArrTablero[2]="O")&&(ArrTablero[4]="O")&&(ArrTablero[6]="O")) {
+//     console.log("El jugador O ha ganado.");
+//   } else if ((ArrTablero[0]="O")&&(ArrTablero[3]="O")&&(ArrTablero[6]="O")) {
+//     console.log("El jugador O ha ganado.");
+//   } else if ((ArrTablero[1]="O")&&(ArrTablero[4]="O")&&(ArrTablero[7]="O")) {
+//     console.log("El jugador O ha ganado.");
+//   } else if ((ArrTablero[2]="O")&&(ArrTablero[5]="O")&&(ArrTablero[8]="O")) {
+//      console.log("El jugador O ha ganado.");
+//   } else {
+//      console.log("La partida continúa.")
+//   }
+
+
+// }
+
+
+
+
+
 console.log("Es el turno de X");
 arrCasillas.map((casillaEscogida) => {
     casillaEscogida.addEventListener("click", () => {
+
+      Comprobarganador (ArrTablero);
+
+    if ((casillaEscogida.innerHTML != "X") && (casillaEscogida.innerHTML != "O")) {
     console.log(casillaEscogida.innerHTML);
     numeroClicks ++;
     // console.log("a ver los clicks", numeroClicks)
@@ -26,17 +93,22 @@ arrCasillas.map((casillaEscogida) => {
       turnosO ++;
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
-      ArrTablero[casillaEscogida.innerHTML] ="X";
+      ArrTablero[casillaEscogida.innerHTML] = "O"; //gracias a esta línea indicamos en el tablero 
+      casillaEscogida.innerHTML = "O";
+      //lógico qué casillas están marcadas por el jugador X
       console.log(ArrTablero);
     } else {
       console.log("Es el turno de O");
       turnosX ++;
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
-      ArrTablero[casillaEscogida.innerHTML] = "O";
+      ArrTablero[casillaEscogida.innerHTML] = "X";
+      casillaEscogida.innerHTML = "X";
       console.log(ArrTablero);
     }
+     } else {};
 
 
-    });
+      });
   });
+
