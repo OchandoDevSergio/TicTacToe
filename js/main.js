aca9a3bb09d8c1a092dd8349970e47ab73786b72
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-07 00:52:29 +0200

    version 1 finalizada a falta de solucionar algunos problemas

--- js/main.js
+++ js/main.js
@@ -11,59 +11,18 @@ let ArrTablero = ["empty", "empty", "empty", "empty", "empty", "empty", "empty",
 const Comprobarganador = (ArrTablero) => {
 
   if (((ArrTablero[0]=="X")&&(ArrTablero[1]=="X")&&(ArrTablero[2]=="X"))||((ArrTablero[3]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[5]=="X"))||((ArrTablero[6]=="X")&&(ArrTablero[7]=="X")&&(ArrTablero[8]=="X"))||((ArrTablero[0]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[8]=="X"))||((ArrTablero[2]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[6]=="X"))||((ArrTablero[0]=="X")&&(ArrTablero[3]=="X")&&(ArrTablero[6]=="X"))||((ArrTablero[1]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[7]=="X"))||((ArrTablero[2]=="X")&&(ArrTablero[5]=="X")&&(ArrTablero[8]=="X"))) {
-     console.log("El jugador X ha ganado.");
+    document.getElementById("asignaganador").innerHTML = "El jugador X ha gando.";//indica que el jugador X ha ganado
     
   } else if (((ArrTablero[0]=="O")&&(ArrTablero[1]=="O")&&(ArrTablero[2]=="O"))||((ArrTablero[3]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[5]=="O"))||((ArrTablero[6]=="O")&&(ArrTablero[7]=="O")&&(ArrTablero[8]=="O"))||((ArrTablero[0]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[8]=="O"))||((ArrTablero[2]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[6]=="O"))||((ArrTablero[0]=="O")&&(ArrTablero[3]=="O")&&(ArrTablero[6]=="O"))||((ArrTablero[1]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[7]=="O"))||((ArrTablero[2]=="O")&&(ArrTablero[5]=="O")&&(ArrTablero[8]=="O"))) {
-     console.log("El jugador O ha ganado.");
+    document.getElementById("asignaganador").innerHTML = "El jugador O ha gando.";
 
   } else {
-     console.log("La partida continúa.")
+    document.getElementById("asignaganador").innerHTML = "La partida continúa.";
   }
 
 
 }
 
-
-// const Comprobarganador = (ArrTablero) => {
-
-//   if ((ArrTablero[0]=="X")&&(ArrTablero[1]=="X")&&(ArrTablero[2]=="X")) {
-//     console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[3]="X")&&(ArrTablero[4]="X")&&(ArrTablero[5]="X")) {
-//     console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[6]="X")&&(ArrTablero[7]="X")&&(ArrTablero[8]="X")) {
-//     console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[0]="X")&&(ArrTablero[4]="X")&&(ArrTablero[8]="X")) {
-//     console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[2]="X")&&(ArrTablero[4]="X")&&(ArrTablero[6]="X")) {
-//     console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[0]="X")&&(ArrTablero[3]="X")&&(ArrTablero[6]="X")) {
-//     console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[1]="X")&&(ArrTablero[4]="X")&&(ArrTablero[7]="X")) {
-//     console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[2]="X")&&(ArrTablero[5]="X")&&(ArrTablero[8]="X")) {
-//      console.log("El jugador X ha ganado.");
-//   } else if ((ArrTablero[0]="O")&&(ArrTablero[1]="O")&&(ArrTablero[2]="O")) {
-//     console.log("El jugador O ha ganado.");
-//   } else if ((ArrTablero[3]="O")&&(ArrTablero[4]="O")&&(ArrTablero[5]="O")) {
-//     console.log("El jugador O ha ganado.");
-//   } else if ((ArrTablero[6]="O")&&(ArrTablero[7]="O")&&(ArrTablero[8]="O")) {
-//     console.log("El jugador O ha ganado.");
-//   } else if ((ArrTablero[0]="O")&&(ArrTablero[4]="O")&&(ArrTablero[8]="O")) {
-//     console.log("El jugador O ha ganado.");
-//   } else if ((ArrTablero[2]="O")&&(ArrTablero[4]="O")&&(ArrTablero[6]="O")) {
-//     console.log("El jugador O ha ganado.");
-//   } else if ((ArrTablero[0]="O")&&(ArrTablero[3]="O")&&(ArrTablero[6]="O")) {
-//     console.log("El jugador O ha ganado.");
-//   } else if ((ArrTablero[1]="O")&&(ArrTablero[4]="O")&&(ArrTablero[7]="O")) {
-//     console.log("El jugador O ha ganado.");
-//   } else if ((ArrTablero[2]="O")&&(ArrTablero[5]="O")&&(ArrTablero[8]="O")) {
-//      console.log("El jugador O ha ganado.");
-//   } else {
-//      console.log("La partida continúa.")
-//   }
-
-
-// }
 
 
 
@@ -88,8 +47,9 @@ arrCasillas.map((casillaEscogida) => {
 
     if (esTurnoX == true) {  //Con este if acumulamos los turnos que han transcurrido de cada jugador
       //e indicamos por consola el turno actual
-      console.log("Es el turno de X");
+      document.getElementById("asignaturnos").innerHTML = "Es el turno del jugador X";//indica que es el turno de X
       turnosO ++;
+      document.getElementById("turnosO").innerHTML = `Turnos de O transcurridos: ${turnosO}`;//indica los turnos de O que ya han transcurrido
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
       ArrTablero[casillaEscogida.innerHTML] = "O"; //gracias a esta línea indicamos en el tablero 
@@ -98,8 +58,9 @@ arrCasillas.map((casillaEscogida) => {
       console.log(ArrTablero);
       Comprobarganador (ArrTablero);
     } else {
-      console.log("Es el turno de O");
+      document.getElementById("asignaturnos").innerHTML = "Es el turno del jugador O";
       turnosX ++;
+      document.getElementById("turnosX").innerHTML = `Turnos de X transcurridos: ${turnosX}`;
       console.log(turnosX, "Son los turnos que X ha tenido.");
       console.log(turnosO, "Son los turnos que O ha tenido.");
       ArrTablero[casillaEscogida.innerHTML] = "X";
@@ -111,7 +72,7 @@ arrCasillas.map((casillaEscogida) => {
 
 
 
-    } else { console.log("La partida ya ha finalizado.");}
+    } else { document.getElementById("asignaganador").innerHTML = "La partida ha finalizado sin un ganador.";}
 
 
       });
