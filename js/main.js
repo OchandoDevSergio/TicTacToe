c170dddd3a8f614444f301e847ecdbbeb3cb372f
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-07 04:50:05 +0200

    finalizado tic tac toe en su version inicial sin ya ningún problema

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ let arrCasillas = Array.from(conjuntoCasillas);
 let numeroClicks = 0;
 let turnosX = 0;
 let turnosO = 0;
-let esTurnoX = true;
+let esTurnoO = true;
+let finJuego = 0;
 let ArrTablero = ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"];
 // console.log(ArrTablero);
 
@@ -12,68 +13,61 @@ const Comprobarganador = (ArrTablero) => {
 
   if (((ArrTablero[0]=="X")&&(ArrTablero[1]=="X")&&(ArrTablero[2]=="X"))||((ArrTablero[3]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[5]=="X"))||((ArrTablero[6]=="X")&&(ArrTablero[7]=="X")&&(ArrTablero[8]=="X"))||((ArrTablero[0]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[8]=="X"))||((ArrTablero[2]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[6]=="X"))||((ArrTablero[0]=="X")&&(ArrTablero[3]=="X")&&(ArrTablero[6]=="X"))||((ArrTablero[1]=="X")&&(ArrTablero[4]=="X")&&(ArrTablero[7]=="X"))||((ArrTablero[2]=="X")&&(ArrTablero[5]=="X")&&(ArrTablero[8]=="X"))) {
     document.getElementById("asignaganador").innerHTML = "El jugador X ha gando.";//indica que el jugador X ha ganado
-    
+    console.log("El jugador X ha ganado");
+    document.getElementById("asignaturnos").innerHTML = "Fin del juego";
+    finJuego = 1;
   } else if (((ArrTablero[0]=="O")&&(ArrTablero[1]=="O")&&(ArrTablero[2]=="O"))||((ArrTablero[3]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[5]=="O"))||((ArrTablero[6]=="O")&&(ArrTablero[7]=="O")&&(ArrTablero[8]=="O"))||((ArrTablero[0]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[8]=="O"))||((ArrTablero[2]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[6]=="O"))||((ArrTablero[0]=="O")&&(ArrTablero[3]=="O")&&(ArrTablero[6]=="O"))||((ArrTablero[1]=="O")&&(ArrTablero[4]=="O")&&(ArrTablero[7]=="O"))||((ArrTablero[2]=="O")&&(ArrTablero[5]=="O")&&(ArrTablero[8]=="O"))) {
     document.getElementById("asignaganador").innerHTML = "El jugador O ha gando.";
-
+    console.log("EL jugador O ha ganado");
+    document.getElementById("asignaturnos").innerHTML = "Fin del juego";
+    finJuego = 1;
   } else {
     document.getElementById("asignaganador").innerHTML = "La partida continúa.";
   }
 
-
 }
 
 
-
-
-console.log("Es el turno de X");
 arrCasillas.map((casillaEscogida) => {
     casillaEscogida.addEventListener("click", () => {
 
-      if (turnosO < 3) {
+  if (finJuego == 0) {
 
     if ((casillaEscogida.innerHTML != "X") && (casillaEscogida.innerHTML != "O")) {
-    // console.log(casillaEscogida.innerHTML); Esto indicaba la casilla que se había pinchado
-    //antes de cambiar su númeor por un espacio.
-    numeroClicks ++;
-    // console.log("a ver los clicks", numeroClicks)
+      numeroClicks ++;
+      let esTurnoO = (numeroClicks%2 == 0)  //La variable esTurnoX será true si es el turno de X y
+      //false si es el turno de O
+        ? true
+        : false;
 
-    let esTurnoX = (numeroClicks%2 == 0)  //La variable esTurnoX será true si es el turno de X y
-    //false si es el turno de O
-      ? true
-      : false;
-
-    //  console.log("valor de esTurnoX", esTurnoX);
-
-    if (esTurnoX == true) {  //Con este if acumulamos los turnos que han transcurrido de cada jugador
-      //e indicamos por consola el turno actual
-      document.getElementById("asignaturnos").innerHTML = "Es el turno del jugador X";//indica que es el turno de X
-      turnosO ++;
-      document.getElementById("turnosO").innerHTML = `Turnos de O transcurridos: ${turnosO}`;//indica los turnos de O que ya han transcurrido
-      console.log(turnosX, "Son los turnos que X ha tenido.");
-      console.log(turnosO, "Son los turnos que O ha tenido.");
-      ArrTablero[casillaEscogida.innerHTML] = "O"; //gracias a esta línea indicamos en el tablero 
-      casillaEscogida.innerHTML = "O";
-      //lógico qué casillas están marcadas por el jugador X
-      console.log(ArrTablero);
-      Comprobarganador (ArrTablero);
-    } else {
-      document.getElementById("asignaturnos").innerHTML = "Es el turno del jugador O";
-      turnosX ++;
-      document.getElementById("turnosX").innerHTML = `Turnos de X transcurridos: ${turnosX}`;
-      console.log(turnosX, "Son los turnos que X ha tenido.");
-      console.log(turnosO, "Son los turnos que O ha tenido.");
-      ArrTablero[casillaEscogida.innerHTML] = "X";
-      casillaEscogida.innerHTML = "X";
-      console.log(ArrTablero);
-      Comprobarganador (ArrTablero);
+      if (esTurnoO == true) {  //Con este if acumulamos los turnos que han transcurrido de cada jugador
+        //e indicamos por consola el turno actual
+        document.getElementById("asignaturnos").innerHTML = "Es el turno del jugador X";//indica que es el turno de X
+        turnosO ++;
+        document.getElementById("turnosO").innerHTML = `Turnos de O transcurridos: ${turnosO}`;//indica los turnos de O que ya han transcurrido
+        ArrTablero[casillaEscogida.id] = "O"; //gracias a esta línea indicamos en el tablero 
+        casillaEscogida.innerHTML = "O";
+        //lógico que casillas están marcadas por el jugador X
+        Comprobarganador (ArrTablero);
+      } else {
+        document.getElementById("asignaturnos").innerHTML = "Es el turno del jugador O";
+        turnosX ++;
+        document.getElementById("turnosX").innerHTML = `Turnos de X transcurridos: ${turnosX}`;
+        ArrTablero[casillaEscogida.id] = "X";
+        casillaEscogida.innerHTML = "X";
+        Comprobarganador (ArrTablero);
+      }
     }
-     } else {};
+    
+    if ((turnosO==3)&&(finJuego==0)) {//gracias a este if nos aseguramos de que se finaliza la partida sin un ganador
+      finJuego=1;
+      document.getElementById("asignaturnos").innerHTML = "Fin del juego";
+      document.getElementById("asignaganador").innerHTML = "La partida ha finalizado sin un ganador.";
+      }
 
 
-
-    } else { document.getElementById("asignaganador").innerHTML = "La partida ha finalizado sin un ganador.";}
-
+    }
+    
 
       });
   });
